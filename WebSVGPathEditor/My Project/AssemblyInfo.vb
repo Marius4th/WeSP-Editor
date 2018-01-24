e88a989fb54b20dd056395a3cbfc6382f7e8aa68
Mario Cool <coolmario1@gmail.com>
2018-01-24 16:58:51 +0100

    version number change

--- WebSVGPathEditor/My Project/AssemblyInfo.vb
+++ WebSVGPathEditor/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.0.0")>
-<Assembly: AssemblyFileVersion("0.1.0.0")>
+<Assembly: AssemblyVersion("0.1.2.0")>
+<Assembly: AssemblyFileVersion("0.1.2.0")>
 <Assembly: NeutralResourcesLanguage("en")>
